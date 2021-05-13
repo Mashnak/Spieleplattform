5457d2812c01b766096e94091baca32655f66122
Markus Schmidgall <markus.schmidgall@googlemail.com>
2021-05-13 22:17:54 +0200

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardComponent } from './components/card/card.component';
+import { BoardComponent } from './components/board/board.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
+    BoardComponent
   ],
   imports: [
     BrowserModule,
