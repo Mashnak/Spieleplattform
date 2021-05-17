e03f4ea33ebe3a448c50e9c006c276e9743d8d5a
Markus Schmidgall <markus.schmidgall@googlemail.com>
2021-05-17 02:36:15 +0200

    Create functionality to give the player object 12 random cards at the start of the game

--- src/app/components/player/player.component.ts
+++ src/app/components/player/player.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { BoardComponent} from '../board/board.component';
+import {iCard} from '../../../interfaces/icard';
+import { RulesOfTheGameService} from '../../services/rulesofthegame.service';
 
 @Component({
   selector: 'app-player',
@@ -7,9 +10,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlayerComponent implements OnInit {
 
-  constructor() { }
+  player_cards: iCard[]= [];
+  constructor(private board: BoardComponent, private rules: RulesOfTheGameService) { }
 
   ngOnInit(): void {
+    for(let i = 0; i < 12; i++) {
+      let random_card: number = Math.floor(Math.random() * this.board.cards.length);
+      this.player_cards.push(this.board.cards[random_card]);
+      // TODO: Delete the 12 cards that the player got from the pile of 150 game cards
+    }
+    console.log('12 Player cards', this.player_cards);
+    console.log('All cards after the player picked 12', this.board.cards);
+    console.log('Card values', this.rules.checkCardValues(this.player_cards));
   }
-
 }
