0371460fa1d9b8cbafaeaa1849d401e75b6164f3
Markus Schmidgall <markus.schmidgall@googlemail.com>
2021-05-17 02:37:50 +0200

    Create a complete card deck at the start of the game. Also add some CSS and HTML to show the players score on the board.

--- src/app/components/board/board.component.ts
+++ src/app/components/board/board.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {RulesOfTheGameService} from '../../services/rulesofthegame.service';
+import {iCard} from '../../../interfaces/icard';
 
 @Component({
   selector: 'app-board',
@@ -7,9 +9,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BoardComponent implements OnInit {
 
-  constructor() { }
+  cards: iCard[] = [];
+  players = [{
+    id: 1,
+    name: 'player1',
+    color: 'red',
+    score: 44,
+    active: true
+  },
+    {
+      id: 2,
+      name: 'player2',
+      color: 'blue',
+      score: 22,
+      active: false
+    },
+    {
+      id: 3,
+      name: 'player3',
+      color: 'green',
+      cards: [{
+        value: 5,
+        turned: true
+      }],
+      score: 111,
+      active: false
+    }]
 
-  ngOnInit(): void {
+  constructor(private rules: RulesOfTheGameService) {
+
   }
 
+  /**
+   * Function
+   */
+  ngOnInit(): void {
+    for (let i = 0; i < 150; i++) {
+      if (i < 5) {
+        this.cards.push({id:i, value: -2, turned: false});
+      }else if (i < 15) {
+        this.cards.push({id:i, value: -1, turned: false});
+      } else if (i < 30) {
+        this.cards.push({id:i, value: 0, turned: false});
+      } else {
+        this.cards.push({id:i, value: Math.floor((i- 20) /10), turned: false});
+      }
+    }
+    console.log('Initialization of the card deck', this.cards);
+  }
 }
